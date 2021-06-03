39f5a7c01c63d6a44ef4237fc972ace3ccbcb493
Joseph Joslin <jjoslin07@gmail.com>
2021-06-02 18:09:45 -0700

    Function to create product card
     This function will be used to create the product cards dynamically on the home page

--- js/main.js
+++ js/main.js
@@ -19,3 +19,26 @@ makeRequest = () => {
         }
     })
 }
+
+// This function will be used to create the product cards dynamically on the home page
+
+createCard = (response) => {
+    const section = document.getElementById('productSection');
+    for (let i in response) {
+        // Create product card elements
+        const article = document.createElement('article');
+        const productCard = document.createElement('div');
+        const cardBody = document.createElement('div');
+        // Add Bootstrap classes to card elements
+        article.classList.add('container-fluid', 'col-lg-3', 'my-2');
+        productCard.classList.add('card', 'mx-auto', 'mt-5');
+        cardBody.classList.add('card-body', 'text-center', 'mx-auto');
+        // Build out the product card using the Teddies API
+        productCard.innerHTML += '<img src="' + response[i].imageUrl + '"alt="" class="mx-auto img-thumbnail" width="auto" height="auto"/>';
+        cardBody.innerHTML += '<h5 class="card-name font-weight-bold">' + response[i].name + '</h5> <p class="card-description text-justify p-1">' + response[i].description + '</p> <p class="card-price">' + '$' + response[i].price / 100 + '</p> </div>' + '<a href="product.html?!id=' + response[i]._id + '"class="btn details px-auto"> view details </a>';
+        // Append completed elemnts to the card
+        cardBody.appendChild(productCard);
+        productCard.appendChild(article);
+        article.appendChild(productSection);
+    }
+}
