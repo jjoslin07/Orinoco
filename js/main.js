f433a9d4500f05fce9303433487bd4909a0df212
Joseph Joslin <jjoslin07@gmail.com>
2021-06-27 10:38:27 -0700

    increase size of product card price text and make font weight bold

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ createCard = (response) => {
         cardBody.classList.add('card-body', 'text-center', 'mx-auto');
         cardImage.innerHTML += '<img class="mx-auto img-top img-fluid rounded-top" style="object-fit:cover; height:100%; width:100%" id="cardImages" src="' + response[i].imageUrl + '"/>';
         cardBody.innerHTML += '<div class="cvp"> <h5 class="card-title font-weight-bold">' + response[i].name + '</h5> <p class="card-description text-justify">' + response[i].description +
-        '</p> <p class="card-price d-flex justify-content-center">' + '$' + response[i].price / 100 + '</p>' + '<a href="product.html?id=' + response[i]._id +
+        '</p> <p class="card-price d-flex justify-content-center font-weight-bold lead">' + '$' + response[i].price / 100 + '</p>' + '<a href="product.html?id=' + response[i]._id +
         ' "class="btn details rounded stretched-link"> View' + ' ' + response[i].name + '  </a> </div>';
         card.appendChild(cardImage);
         cardImage.appendChild(cardBody);
