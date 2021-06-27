6218466c5aa5c82e501ca3d69f96c6dd1ca02ca7
Joseph Joslin <jjoslin07@gmail.com>
2021-06-26 23:13:44 -0700

    Return

--- js/cart.js
+++ js/cart.js
@@ -5,6 +5,7 @@ let orderId;
 let localStorageContent = localStorage.getItem('cart');
 let cartItemsArray = JSON.parse(localStorageContent);
 
+// Update cart number in navigation menu
 function addNumCart() {
     if (localStorageContent) {
         let cartItemsTotal = document.querySelector('.cartItemsTotal');
@@ -13,11 +14,14 @@ function addNumCart() {
 }
 
 addNumCart();
+// Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
     'use strict'
 
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.querySelectorAll('.needs-validation')
 
+    // Loop over them and prevent submission
     Array.prototype.slice.call(forms)
         .forEach(function (form) {
             form.addEventListener('submit', function (event) {
@@ -29,7 +33,10 @@ addNumCart();
                     event.preventDefault()
                     event.stopPropagation()
                     console.log('true');
+
                     let products = [];
+
+                    // Get product id and push it in array
                     let cartArray = JSON.parse(localStorage.getItem('cart'));
                     for (let i = 0; i < cartArray.length; i++) {
                         products.push(cartArray[i].id);
@@ -41,6 +48,7 @@ addNumCart();
                     let country = document.getElementById('country');
                     let city = document.getElementById('city');
                     let zip = document.getElementById('zip');
+                    // Object stores informations from form
                     let contact = {
                         firstName: firstName.value,
                         lastName: lastName.value,
@@ -64,22 +72,28 @@ addNumCart();
         })
 })()
 
+//CART
 function displayCart() {
     let localStorageContent = localStorage.getItem('cart');
     let cartItemsArray = JSON.parse(localStorageContent);
     if (cartItemsArray) {
         for (let i = 0; i < cartItemsArray.length; i++) {
+
             let cart = document.getElementById('shoppingCart');
             let cartItem = document.createElement('li');
+
             cartItem.classList.add('cart-row', 'list-group-item', 'd-flex', 'justify-content-between', 'lh-sm');
             cartItem.style.height = '75px';
+
             let item = document.createElement('div');
             item.classList.add('w-50');
             item.innerHTML = '<p class="my-0">' + cartItemsArray[i].name + '</p> <small class="text-muted">' + cartItemsArray[i].selectColors + '</small>';
+
             let cost = document.createElement('div');
             cost.classList.add('w-25');
             cost.style.marginTop = '12px';
             cost.innerHTML = '<p class="cart-price text-bold">' + '$' + cartItemsArray[i].price / 100 + '</p>';
+
             let removeButton = document.createElement('button');
             removeButton.setAttribute('type', 'button');
             removeButton.classList.add('btn','my-2', 'py-1', 'px-2', 'remove');
@@ -91,6 +105,7 @@ function displayCart() {
                 localStorage.setItem('cart', JSON.stringify(cartItemsArray));
                 location.reload();
             }
+
             cart.appendChild(cartItem);
             cartItem.appendChild(item);
             cartItem.appendChild(cost);
@@ -101,12 +116,14 @@ function displayCart() {
 
 displayCart();
 
+//calculate total cost
 function updateCartTotal() {
     let total = document.getElementById('totalCost');
     let totalCost = 0
     for (let i = 0; i < cartItemsArray.length; i++) {
         totalCost += cartItemsArray[i].price / 100;
     }
+    //return totalCost
     total.innerHTML = '$' + totalCost;
 
     sessionStorage.setItem('price', totalCost);
@@ -114,6 +131,7 @@ function updateCartTotal() {
 
 updateCartTotal();
 
+// Send inforamtion from user to api go to confirmation page
 function makeRequest(data) {
     fetch('http://localhost:3000/api/teddies/order', {
         method: 'POST',
--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-/**
- * This function is used to make an Api request from the backend-server
- */
+// This function is used to make an Api request from the backend-server
 
 makeRequest = () => {
     return fetch("http://localhost:3000/api/teddies")
@@ -10,7 +8,7 @@ makeRequest = () => {
         .then(response => {
             createCard(response)
         })
-        .catch(function () {
+        .catch(function (error) {
             alert(
                 "We\'re sorry the server is unavailable"
             )
@@ -24,32 +22,34 @@ callForApi = async () => {
 
 callForApi();
 
-/**
- * This function will be used to create the product cards dynamically on the home page
- */
+// This function will be used to create the product cards dynamically on the home page
 
 createCard = (response) => {
     const section = document.querySelector('#productSection');
     for (let i in response) {
+        // Create product card elements
         const card = document.createElement('article');
         const cardImage = document.createElement('div');
         const cardBody = document.createElement('div');
+        // Add classes to card elements
         card.classList.add('col-lg-3', 'col-md-4', 'col-sm-6','my-2', 'd-flex');
-        cardImage.classList.add('card', 'mx-auto', 'p-auto', 'productCard');
+        cardImage.classList.add('card', 'mx-auto', 'p-auto');
         cardBody.classList.add('card-body', 'text-center', 'mx-auto');
+        // Add Style to article element
+        // Build out the product card using the Teddies API
         cardImage.innerHTML += '<img class="mx-auto img-top img-fluid rounded-top" style="object-fit:cover; height:100%; width:100%" id="cardImages" src="' + response[i].imageUrl + '"/>';
         cardBody.innerHTML += '<div class="cvp"> <h5 class="card-title font-weight-bold">' + response[i].name + '</h5> <p class="card-description text-justify">' + response[i].description +
         '</p> <p class="card-price d-flex justify-content-center">' + '$' + response[i].price / 100 + '</p>' + '<a href="product.html?id=' + response[i]._id +
         ' "class="btn details rounded stretched-link"> View' + ' ' + response[i].name + '  </a> </div>';
+        // Append compeleted Card Elements
         card.appendChild(cardImage);
         cardImage.appendChild(cardBody);
         section.appendChild(card);
     }
 }
 
-/**
- * Function to update items number in shopping cart.
- */
+
+// Function to update items number in shopping cart.
 
 function addNumCart() {
     const localStorageContent = localStorage.getItem('cart');
