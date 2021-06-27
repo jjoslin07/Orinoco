170c4cabc2b55263cb4876b15be02109026b3a0b
Joseph Joslin <jjoslin07@gmail.com>
2021-06-26 23:23:34 -0700

    update confirmation.js documentation

--- js/confirmation.js
+++ js/confirmation.js
@@ -3,16 +3,10 @@
 let thankYouName = document.querySelector('.thankYouName');
 
 thankYouName.innerHTML = ' ' + sessionStorage.getItem('firstName') + '!';
-
-// Dom Element References 
 let totalCost = document.getElementById('totalCost');
 let orderId = document.getElementById('orderId');
-
-// Shows total cost of order and order ID
 totalCost.innerHTML = '$' + ' ' + sessionStorage.getItem('price');
 orderId.innerHTML = sessionStorage.getItem('orderId');
-
-// remove the item from localStorage and sessionStorage
 document.getElementById('returnToHomePage').addEventListener('click', eraseSessionStorage);
 function eraseSessionStorage() {
     sessionStorage.removeItem('orderId');
